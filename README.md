8647891017f7c68ca146036ba4e0a106265cb44f
ozelotjp <contact@ozelot.app>
2018-12-19 18:22:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# ozelotjp-box/youtube.com
+
+youtube.com用のUserScript
+
+## 機能
+
+- 動画視聴ページの再生リストが非表示に出来るように
+
+## キーボードショートカット
+
+| Key | Description             |
+| --- | ----------------------- |
+| p   | 再生リストを表示/非表示 |
+
+※動画視聴ページを開いた時に表示されるメッセージをクリックしても、再生リストは非表示になります。
+
+## インストール
+
+1. Violentmonkeyをダウンロード ([Chrome][violentmonkey-chrome], [Firefox][violentmonkey-firefox])
+2. app.user.jsを開く ([こちら][app.user.js])
+3. 右上にある **インストール** をクリック
+
+※Violentmonkey以外は未検証です
+
+[violentmonkey-chrome]: https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag
+[violentmonkey-firefox]: https://addons.mozilla.org/ja/firefox/addon/violentmonkey/
+[app.user.js]: https://github.com/ozelotjp-box/youtube.com/raw/master/app.user.js
---
+++ app.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name ozelotjp-box/youtube.com
+// @version 1.9
+// @author ozelotjp
+// @homepageURL https://github.com/ozelotjp-box/youtube.com
+// @downloadURL https://github.com/ozelotjp-box/youtube.com/raw/master/app.user.js
+// @match https://www.youtube.com/watch
+// @grant GM_notification
+// ==/UserScript==
+
+function reversePlaylist() {
+  let playlist = document.getElementById("playlist");
+  if (playlist.style.display === "") {
+    playlist.style.display = "none";
+  } else {
+    playlist.style.display = "block";
+  }
+}
+
+GM_notification("プレイリストの非表示はココをクリック\nまたは p キーを押してください\n\n(C) 2018 ozelotjp", "ozelotjp/hide-playlist からのお知らせ", "https://i.imgur.com/tUwnLok.png", (function () {
+  reversePlaylist();
+}));
+
+document.onkeydown = (function (event) {
+  if (event.key === "p") {
+    reversePlaylist();
+  }
+});
