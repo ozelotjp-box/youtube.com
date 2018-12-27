02b397cc7ed197dbe63921dde9c7a6c2e2ac7f6a
ozelot <ozelot318@gmail.com>
2018-12-28 00:27:31 +0900

    Update app.user.js

--- app.user.js
+++ app.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name ozelotjp-box/youtube.com
-// @version 1.13
+// @version 1.14
 // @author ozelotjp
 // @homepageURL https://github.com/ozelotjp-box/youtube.com
 // @downloadURL https://github.com/ozelotjp-box/youtube.com/raw/master/app.user.js
-// @match https://www.youtube.com/*
+// @match https://www.youtube.com/watch
 // @grant GM_notification
 // ==/UserScript==
 
@@ -17,9 +17,11 @@ function reversePlaylist() {
   }
 }
 
-GM_notification("プレイリストの非表示はココをクリック\nまたは p キーを押してください\n\n(C) 2018 ozelotjp", "ozelotjp/hide-playlist からのお知らせ", "https://i.imgur.com/tUwnLok.png", (function () {
-  reversePlaylist();
-}));
+if (location.href.match(/https:\/\/www\.youtube\.com\/watch/gm)) {
+  GM_notification("プレイリストの非表示はココをクリック\nまたは p キーを押してください\n\n(C) 2018 ozelotjp", "ozelotjp/hide-playlist からのお知らせ", "https://i.imgur.com/tUwnLok.png", (function () {
+    reversePlaylist();
+  }));
+}
 
 document.onkeydown = (function (event) {
   if (event.key === "p") {
